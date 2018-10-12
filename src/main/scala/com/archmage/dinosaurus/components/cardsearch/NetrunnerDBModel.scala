7c0f864d70fe91f5313fd9e52a448eecf5ec41af
Kara Valentine <gitkraken@archmage.me>
2018-10-12 13:49:26 +1100

    Fix some broken search logic, remove a typo on Dino strength buff text

--- src/main/scala/com/archmage/dinosaurus/components/cardsearch/NetrunnerDBModel.scala
+++ src/main/scala/com/archmage/dinosaurus/components/cardsearch/NetrunnerDBModel.scala
@@ -3,7 +3,6 @@ package com.archmage.dinosaurus.components.cardsearch
 import com.archmage.dinosaurus.globals.Constants
 
 import scala.io.Source
-import scala.util.matching.Regex
 
 object NetrunnerDBModel {
   val cards: List[NetrunnerDBCard] = {
@@ -20,17 +19,13 @@ object NetrunnerDBModel {
   }
 
   def searchCard(searchText: String): List[NetrunnerDBCard] = {
-    val matches = cards.filter(card => card.title.toLowerCase.contains(searchText.toLowerCase))
+    var matches = cards.filter(card => card.title.toLowerCase == searchText.toLowerCase)
+    if(matches.isEmpty) matches = cards.filter(card => card.title.toLowerCase.contains(searchText.toLowerCase))
 
     // for duplicates, pick the latter (revised core)
     // I like the newer instances, they're more recent!
     if(matches.size == 2 && matches.head.title == matches(1).title) {
       matches.slice(1, 2)
-    }
-
-    // if there is exactly one that matches perfectly, return it (obviously!)
-    else if(matches.exists(card => card.title.toLowerCase == searchText.toLowerCase)) {
-      matches.filter(card => card.title.toLowerCase == searchText.toLowerCase)
     }
 
     else matches
